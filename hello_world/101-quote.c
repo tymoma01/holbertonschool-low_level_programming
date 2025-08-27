7d66e946c8000b7aa3c9797c9a755c882f3a4fef
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-27 11:50:36 +1000

    101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, msg, sizeof(msg) - 1);
+return (1);
+}
