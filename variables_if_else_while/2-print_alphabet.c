d39078a5452004107dc3431e2e1c903a44fb505f
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-27 14:40:05 +1000

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main function to identify positive, negative or nul for random number
+ * Return: Always 0 (Success)
+**/
+int main(void)
+{
+char lettre;
+for (lettre = 'a'; lettre<'z'; lettre++)
+{
+putchar(lettre);
+}
+putchar('\n');
+return (0);
+}
