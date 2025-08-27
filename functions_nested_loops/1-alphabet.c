72e680004ba2717be3efde75a897f16994092f3a
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-27 16:53:25 +1000

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
