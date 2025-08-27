9a9a27d1ec1d9c746fc11422a5711525a028c483
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-27 16:31:37 +1000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints all possible combinations of single-digit numbers
+ * main - prints "_putchar" followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+
 	return (0);
 }
