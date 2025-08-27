39db4e56ffc7199fa13b298a6b5b1d71bf1b8d3f
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-27 14:57:43 +1000

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main function to print the alphabet
+ * Return: Always 0 (Success)
+**/
+int main(void)
+{
+int digit;
+for (digit = 0; digit<10; digit++)
+{
+printf("%d", digit);
+}
+printf("\n");
+return (0);
+}
