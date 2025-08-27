c0c86ed6a49b159ba3a88a1c2b43a76d29421cf6
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-27 11:27:03 +1000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,5 +11,4 @@ printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-return (0);
-}
+return (0);}
