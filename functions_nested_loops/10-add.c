13fab4b01db21076b8014e05ca6214b26f99d258
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-28 12:58:38 +1000

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * main - prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int n , int m )
+{
+	return n + m;
+}
