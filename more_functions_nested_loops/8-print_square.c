6270813c48271bac93d2b528f467c031023a4c69
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-28 16:01:48 +1000

    8-9

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+void print_square(int size)
+{
+    int i, j;
+    if (size > 0){
+        for (i = 0; i < size; i++){
+            for (j = 0; j < size; j++){
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+    else {
+        _putchar('\n');
+    }
+}
