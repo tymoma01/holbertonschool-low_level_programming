266730e9565df15a9f18d286bb500c6b29fe74bd
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-28 15:51:51 +1000

    6-7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+void print_diagonal(int n)
+{
+    int i, j;
+    if (n > 0){
+        for (i = 0; i < n; i++){
+            for (j = 0; j < i; j++){
+                _putchar(' ');
+            }
+            _putchar('\\');
+            _putchar('\n');
+        }
+   }
+   else {
+        _putchar('\n');
+   }
+}
