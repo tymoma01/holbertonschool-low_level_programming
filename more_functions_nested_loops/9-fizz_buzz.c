d85b2017c9e4203ea875b1384c0cf67aa5911247
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-28 16:08:47 +1000

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,19 +4,18 @@ int main(void)
 {
     int i;
     for (i = 1; i <= 100; i++){
-        if (i % 3 == 0 && i % 5 == 0){
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0){
+        if (i % 3 == 0){
             printf("Fizz");
         }
-        else if (i % 5 == 0){
+        if (i % 5 == 0){
             printf("Buzz");
         }
-        else {
+        if (i % 3 != 0 && i % 5 != 0){
             printf("%d", i);
         }
-        printf(" ");
+        if (i != 100){
+            printf(" ");
+        }
     }
     printf("\n");
     return(0);
