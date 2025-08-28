a4bb79b303233c505bc9cebe2f148f22ef5cf256
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-28 16:16:00 +1000

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+void print_triangle(int size)
+{
+    int i, j, k;
+    if (size > 0){
+        for (i = 0; i < size; i++){
+            for (j = size-i; j >0 ; j--){
+                _putchar(' ');
+            }
+            for (k = 0; k <= i; k++){
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+    _putchar('\n');
+}
