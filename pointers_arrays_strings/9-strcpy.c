4df53c5ae2d3af273dc5576892a57b4a2537ff42
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-29 16:31:11 +1000

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strcpy(char *dest, char *src){
+    int i = 0;
+
+    while (src[i] != '\0'){
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
