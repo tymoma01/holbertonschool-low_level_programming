d42f569fe6cad145d892e85547ff8fbe2f365f97
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-29 16:26:26 +1000

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,6 +3,10 @@
 
 void print_array(int *a, int n){
     int i;
+    if (n <= 0){
+        printf("\n");
+        return;
+    }
     printf("%d", a[0]);
     for (i = 1; i < n; i++){
         printf(", %d", a[i]);
