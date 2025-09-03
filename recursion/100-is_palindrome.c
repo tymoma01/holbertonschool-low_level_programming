d730a2e3782475482b970797d5eb7707f2fbfd01
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-03 11:09:13 +1000

    100

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+        return 0;
+    return 1 + _strlen_recursion(s + 1);
+}
+
 int is_palindrome_helper(char *s, int n, int i){
     if (i >= n/2){
         return 1;
@@ -11,12 +18,9 @@ int is_palindrome_helper(char *s, int n, int i){
 }
 
 int is_palindrome(char *s){
-    int len = 0;
     if (*s == '\0'){
         return 1;
     }
-    while (s[len] != '\0'){
-        len++;
-    }
-    return is_palindrome_helper(s, len, 0);
+    
+    return is_palindrome_helper(s, _strlen_recursion(s), 0);
 }
