887c0ad2ee6b31c7000b0e83ef3de39dce6d659c
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-04 10:38:00 +1000

    2

--- malloc_free/2-main.c
+++ malloc_free/2-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    char *s;
+    char *s, *a, *b, *c;
 
     s = str_concat("Best ", "School");
     if (s == NULL)
@@ -18,6 +18,12 @@ int main(void)
         return (1);
     }
     printf("%s\n", s);
+    a = str_concat("Hello", NULL); 
+    printf("%s\n", a);
+    b = str_concat(NULL, "Hello"); 
+    printf("%s\n", b);
+    c = str_concat(NULL, NULL);
+    printf("%s\n", c);
     free(s);
     return (0);
 }
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,6 +9,12 @@ char *str_concat(char *s1, char *s2){
    if (s1 == NULL && s2 == NULL){
     return (NULL);
    }
+   if (s1 == NULL){
+    s1 = "";
+   }
+   if (s2 == NULL){
+    s2 = "";
+   }
 
    while (s1[len1] != '\0'){
     len1++;
