4600377b4e0056c24ce80c39322d231640280cf8
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-04 16:03:54 +1000

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,21 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-void *_calloc(unsigned int nmemb, unsigned int size){
-    int *res;
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    void *res;
     unsigned int i;
-    if (nmemb == 0 || size == 0){
-        return (NULL);
-    }
-    res = malloc((nmemb + 1)*size);
-    if (res == NULL) {
-        return (NULL);
-    }
 
-    for (i = 0; i < nmemb; i++) {
-        res[i] = 0;
-    }
-    res[nmemb] = '\0';
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+
+    if (nmemb > UINT_MAX / size)
+        return (NULL);
+
+    res = malloc(nmemb * size);
+    if (res == NULL)
+        return (NULL);
+
+    for (i = 0; i < nmemb * size; i++)
+        ((unsigned char *)res)[i] = 0;
+
     return res;
 }
