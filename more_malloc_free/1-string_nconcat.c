7a8271447354a476650d51912bbfe5ae61b1192d
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-04 15:43:21 +1000

    1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n){
+    char *res;
+    unsigned int l1 = 0, l2 = 0, i = 0;
+    if (s1 == NULL) {
+        s1 = "";
+    }
+    if (s2 == NULL) {
+        s2 = "";
+    }
+    while (s1[l1] != '\0') {
+        l1++;
+    }
+    while (s2[l2] != '\0') {
+        l2++;
+    }
+    if (n > l2) {
+        n = l2;
+    }
+
+    res = malloc((l1 + l2 + 1) * sizeof(char));
+    if (res == NULL) {
+        return (NULL);
+    }
+    for (i = 0; i < l1; i++) {
+        res[i] = s1[i];
+    }
+    for (i = 0; i < n; i++) {
+        res[l1 + i] = s2[i];
+    }
+    return (res);
+}
