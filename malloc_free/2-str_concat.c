948a67cdfc4389d35201ee226c43d41c2da0ad50
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-04 10:39:50 +1000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,9 +6,6 @@ char *str_concat(char *s1, char *s2){
    int len1 = 0 , len2 = 0, i;
    char *s;
 
-   if (s1 == NULL && s2 == NULL){
-    return (NULL);
-   }
    if (s1 == NULL){
     s1 = "";
    }
