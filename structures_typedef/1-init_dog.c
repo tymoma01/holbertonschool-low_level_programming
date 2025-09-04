3382dc23de1166e4afa3199e9e47edb3e92c6a9b
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-04 17:52:10 +1000

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,8 +1,10 @@
 #include "dog.h"
+#include <stdio.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner){
-
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+    if (d != NULL) {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
 }
