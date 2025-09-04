d882697107c3e30f96c2ae4fb249e88c4657334b
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-04 10:33:48 +1000

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2){
+   int len1 = 0 , len2 = 0, i;
+   char *s;
+
+   if (s1 == NULL && s2 == NULL){
+    return (NULL);
+   }
+
+   while (s1[len1] != '\0'){
+    len1++;
+   }
+   while (s2[len2] != '\0'){
+    len2++;
+   }
+
+   s = malloc((len1 + len2 + 1) * sizeof(char));
+   for (i = 0; i < len1; i++){
+    s[i] = s1[i];
+   }
+   for (i = 0; i < len2; i++){
+    s[len1 + i] = s2[i];
+   }
+   s[len1 + len2] = '\0';
+   return (s);
+}
