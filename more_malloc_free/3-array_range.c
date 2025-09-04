8815e2bf01ce2f3235f1df4dbc1fa9550706aaed
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-04 16:09:35 +1000

    3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+
+int *array_range(int min, int max){
+    int length, i;
+    int *res;
+    if (min > max){
+        return (NULL);
+    }
+    length = max - min + 1;
+
+    res = malloc(length * sizeof(int));
+    if (res == NULL) {
+        return (NULL);
+    }
+    for (i = 0; i < length; i++) {
+        res[i] = min + i;
+    }
+    res[length] = '\0';
+    return (res);
+}
